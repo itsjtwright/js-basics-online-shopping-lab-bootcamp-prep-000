29df61bc4d48776ee6a728f1e5aa33960406c2a3
Jacob Wright <mee@mailinator.com>
2017-12-17 02:48:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function placeOrder(cardNumber) {
   // write your code here
   getCart().length = 0;
   if(typeof cardNumber === Number) {
-    console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`)
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   } else {
     console.log("Sorry, we don't have a credit card on file for you.")
   }
